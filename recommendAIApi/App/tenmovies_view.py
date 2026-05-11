87a4320f9fdb746d5df00c3e2c6cab41e75e8f48
Bryant <thiswasbrii@gmail.com>
2026-05-10 20:14:35 -0400

    ✨Feat : retriving 10 random popular movies

--- recommendAIApi/App/tenmovies_view.py
+++ recommendAIApi/App/tenmovies_view.py
@@ -1,28 +1,44 @@
 from django.http import JsonResponse
 from .models import Movie, Interaction
-from django.db.models import Q
+import random
+
 
 def tenmovies(request, idUser):
-    if request.method == 'GET':
-        # Get a list of movie IDs that the user has already interacted with
-        user_interacted_movie_ids = Interaction.objects.filter(user__id=idUser).values_list('movie_id', flat=True)
-
-        # Get 10 random movies that the user has not interacted with
-        movies_to_recommend = Movie.objects.exclude(id__in=user_interacted_movie_ids).order_by('?')[:10]
-
-        # Prepare the data to return
-        movies_data = [{
-            'id': movie.id,
-            'title': movie.title,
-            'overview': movie.overview,
-            'release_year': movie.release_year,
-            'genres': movie.genres,
-            'popularity': movie.popularity,
-            'vote_average': movie.vote_average,
-            'poster_path': movie.poster_path
-        } for movie in movies_to_recommend]
-
-        return JsonResponse(movies_data, safe=False)
-
-    else:
+    if request.method != 'GET':
         return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+    # Get movie IDs already classified/interacted by the user
+    user_interacted_movie_ids = Interaction.objects.filter(
+        user_id=idUser
+    ).values_list('movie_id', flat=True)
+
+    # First, get a pool of popular movies that the user has not interacted with.
+    # We take more than 10 so the final result can still be random.
+    popular_movies_pool = list(
+        Movie.objects
+        .exclude(id__in=user_interacted_movie_ids)
+        .filter(
+            popularity__gt=0,
+            vote_average__gt=0
+        )
+        .order_by('-popularity', '-vote_average')[:200]
+    )
+
+    # Select 10 random movies from the popular pool
+    movies_to_recommend = random.sample(
+        popular_movies_pool,
+        min(10, len(popular_movies_pool))
+    )
+
+    movies_data = [{
+        'id': movie.id,
+        'title': movie.title,
+        'overview': movie.overview,
+        'release_year': movie.release_year,
+        'genres': movie.genres,
+        'popularity': movie.popularity,
+        'vote_average': movie.vote_average,
+        'poster_path': movie.poster_path
+    } for movie in movies_to_recommend]
+
+    return JsonResponse(movies_data, safe=False)
